1fad2c91133bd42dd8c4aab4a2eb4b4125a4ff99
Daniel Pinna <danielpinna2@gmail.com>
2020-09-19 17:56:44 -0300

    Add and setup initial swagger conf

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
 
 import routes from './routes';
 import './database';
+import * as swaggerDocument from './swagger.json';
 
 const app = express();
 
+app.use(express.json());
 app.use(routes);
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(8000, () => {
   console.log('Server started on port 8000!');
